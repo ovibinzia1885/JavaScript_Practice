466a890d9e1790b413102e6289a252cff478d38a
x37a27 <nazmul.ovy@edu.fh-joanneum.at>
2025-12-13 00:04:22 +0600

    Initial commit

---
+++ javaScriptVariable.js
@@ -0,0 +1,21 @@
+// Variable= A container that store a value which can be changed during the execution of a program. In JavaScript, 
+// variables are declared using keywords like var, let, or const. For example:
+
+
+// Declaration let X;
+// Assignment x=100
+
+// let age=25;
+// let name="Nazmul";
+// let Price=34.66;
+
+// console.log('the age is ${ age }');
+// console.log('my name is:', name);
+// console.log('the price is:', Price);
+
+ LET x=34;
+ console.log(x);
+// In this example, we declare three variables: age, name, and Price, and assign them values of different data types (number and string).
+// We then use console.log() to print the values of these variables to the console.
+
+
