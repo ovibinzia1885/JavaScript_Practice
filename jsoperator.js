0a2fa5ad36b1304c605eb3cecbf4ed06dde4b652
x37a27 <nazmul.ovy@edu.fh-joanneum.at>
2025-12-14 00:37:06 +0600

    JavaScript ARITHMETIC OPERATORS

---
+++ jsoperator.js
@@ -0,0 +1,26 @@
+let values=43;
+
+// value= values+5;
+// value=values-2;
+// value=values*2;
+// value=values/3;
+// values=values**4; // values=values to the power of 4
+// values=values % 6; // values= the remainder of values divided by 6
+
+
+// values +=4; // values=values+4
+// values -=2; // values=values-2
+// values *=3; // values=values*3
+// values /=2; // values=values/2 
+// values %=5; // values=values%5
+// values++; // values=values+1
+// values--; // values=values-1 
+
+// operator procedure 
+// 1. Parentheses ()
+// 2. Exponents **
+// 3. Multiplication * , Division / , Modulus %
+// 4. Addition + , Subtraction -
+let result= 3 + 5 * 2 - (8 / 4) **2;
+
+console.log(result);
