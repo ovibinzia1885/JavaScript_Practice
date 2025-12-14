825cfe08d7e14001ef39aaa305a2e75d1cff8042
x37a27 <nazmul.ovy@edu.fh-joanneum.at>
2025-12-14 13:51:08 +0600

    feat:Relational and Equality Operators

---
+++ relational-operator.js
@@ -0,0 +1,12 @@
+let x=1;
+console.log(x=='1');  //true
+console.log(x==='1'); //false
+
+
+var y=null;
+var z=undefined;    
+console.log(y==z);  //true
+console.log(y===z); //false
+
+var value= 4>5;
+console.log(x); //false
