5ceb25d32317d1ae11531aa22e504f2c7fd5b58c
x37a27 <nazmul.ovy@edu.fh-joanneum.at>
2025-12-14 09:36:39 +0600

    USER INPUT

---
+++ Lesson3_input.js
@@ -0,0 +1,5 @@
+const prompt = require("prompt-sync")();
+
+let age = prompt("Enter your age: ");
+var name=prompt("Enter your name: ");
+console.log("Your age is:", age, "and your name is:", name);
