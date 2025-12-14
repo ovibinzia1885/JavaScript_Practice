98b9e94f1002fc064ea795b17c88a66bc4d64147
x37a27 <nazmul.ovy@edu.fh-joanneum.at>
2025-12-14 10:50:04 +0600

    test: javascript Array

---
+++ array-methods.js
@@ -0,0 +1,31 @@
+let airPlan=["airbus","boeing","cessna","concorde"];
+
+
+// airPlan[1]="boeing 747"; //modifying array element
+// console.log(airPlan);
+
+// airPlan.push("antonov"); //adding element at the end
+// airPlan.pop(); //removing last element
+// airPlan.unshift("lockheed martin"); //adding element at the beginning
+// airPlan.shift(); //removing first element  
+
+
+// let numOfElements=airPlan.length; //length of the array
+// let index=airPlan.indexOf("cessna"); //index of an element
+
+// airPlan.sort(); //sorting array elements
+// airPlan.sort().reverse(); //sorting in reverse order
+
+// console.log(airPlan);
+// console.log(numOfElements);  
+
+for(let i=0;i<airPlan.length;i++){ //looping through array elements
+    console.log(airPlan[i]);
+}
+
+
+
+
+//accessing array elements
+// console.log(airPlan[0]);
+// console.log(airPlan[2]);
