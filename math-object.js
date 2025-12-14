1d6d735b02b24d78ed36e0c78b83253294097b0b
x37a27 <nazmul.ovy@edu.fh-joanneum.at>
2025-12-14 14:06:09 +0600

    feat: math object

---
+++ math-object.js
@@ -0,0 +1,14 @@
+let x=4.45
+let y=2.33
+let z= 34;
+
+// z=Math.round(x);
+// z= Math.floor(x);
+// z= Math.ceil(x);
+// z= Math.trunc(x);
+// z= x.toFixed(1);
+// z=Math.sqrt(y);
+// z= Math.pow(x,y);
+z=Math.min(4,5,6,7,8,9,-1,0);
+// z=Math.max(4,5,6,7,8,9,-1,0);   
+console.log(z); //4
