ed0e877a1043c452120ae82ff8e65d75d9648a9c
x37a27 <nazmul.ovy@edu.fh-joanneum.at>
2025-12-14 09:55:28 +0600

    TYPE CONVERSION

---
+++ Lesson4_TypeCon.js
@@ -0,0 +1,11 @@
+let x= "Nazmul";
+let y= "Nazmul";
+let z= "Nazmul";
+
+x=Number(x);
+y=String(y);
+z=Boolean(z);
+
+console.log("The value of x is:", x, "and the type of x is:", typeof(x));
+console.log("The value of y is:", y, "and the type of y is:", typeof(y));
+console.log("The value of z is:", z, "and the type of z is:", typeof(z));
