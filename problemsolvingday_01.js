b2107ed639c6c7b58e12510dbab39e61cb9a41c3
x37a27 <nazmul.ovy@edu.fh-joanneum.at>
2025-12-19 23:28:39 +0600

    feat:problem solving day 01

---
+++ problemsolvingday_01.js
@@ -0,0 +1,9 @@
+
+let parameterVariable="Welcome to 10 Days of JavaScript!"
+function greeting(parameterVariable) {
+    console.log("Hello, World!");
+    console.log(parameterVariable);
+}
+
+
+greeting(parameterVariable);
