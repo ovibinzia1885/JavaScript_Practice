3d58e0338eff32b4360f120a7ee81d9b817be3bf
x37a27 <nazmul.ovy@edu.fh-joanneum.at>
2025-12-21 01:04:04 +0600

    feat:hacker rank problem solving day 02

---
+++ HR_problemsolving2.js
@@ -0,0 +1,17 @@
+let secondInteger=12;
+let secondDecimal=4.32;
+let secondString="is the best place to learn and practice coding!"
+
+function performOperation(secondInteger, secondDecimal, secondString) {
+
+    const firstInteger = 4;
+    const firstDecimal = 4.0;
+    const firstString = 'HackerRank ';
+    
+    console.log(firstInteger+parseInt(secondInteger))
+    console.log(firstDecimal+parseFloat(secondDecimal))
+    console.log(firstString+secondString)
+   
+}
+performOperation(secondInteger, secondDecimal, secondString);
+
